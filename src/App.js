59deb45f771c5477327520a80afbff85b61dfd25
Julian <thebarbershaveshimself@172-29-247-217.dynapool.nyu.edu>
2019-05-07 14:41:40 -0400

    n player version

--- src/App.js
+++ src/App.js
@@ -2,26 +2,52 @@ import React from 'react';
 import Player from './components/player'
 import './App.css';
 
-const compare = (a, b) => {
-  return a == b ? "It's a tie" : a > b ? "Player 1 is the winner" : "Player 2 is the winner"
+const compare = arr => {
+  let winner = 0
+  let tie = false
+  arr.slice(1).forEach((v, i) => {
+    if (arr[winner] < v) {
+      winner = i+1
+      tie = false
+    } else if (arr[winner] == v) {
+      tie = true
+    }
+  })
+  return [winner, tie]
 }
 
-const App = () => {
+const genScoreboardMessage = ([winner, tie]) => {
+  if (tie) {
+    return "It's a tie!"
+  } else {
+    return "Player " + winner + " wins!"
+  }
+}
+
+const range = n => Array.from(Array(n).keys())
+
+const players = (howManyPlayers, parent) => {
+  return range(howManyPlayers).map((i)=>
+    <Player id={i} parent={parent}/>)
+}
+
+const GenericApp = (howManyPlayers) => {
   const component = new React.Component()
   component.state = {
-    players: [0, 0]
+    players: Array(howManyPlayers).fill(0)
   }
 
   component.render = () => {
     return (
       <div>
-        {compare(component.state.players[0], component.state.players[1])}
-        <Player id={0} parent={component}/>
-        <Player id={1} parent={component}/>
+        {genScoreboardMessage(compare(component.state.players))}
+        {players(howManyPlayers, component)}
       </div>
     )
   }
   return component
 }
+
+const App = () => GenericApp(10)
 
 export default App;
--- src/components/player.js
+++ src/components/player.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 const Player = (props) => {
   const handleClick = (n) => {
-    console.log(props.key)
-    console.log(props.parent.players)
-
     props.parent.setState(prevState => {
       prevState.players[props.id] += n
       return prevState
