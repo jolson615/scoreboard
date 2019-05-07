83188a023c91565947aa8f694378665850de2b14
Julian <thebarbershaveshimself@172-29-247-217.dynapool.nyu.edu>
2019-05-07 15:48:58 -0400

    HARD

--- src/App.js
+++ src/App.js
@@ -6,25 +6,31 @@ const genScoreboardMessage = winner => {
   return "Player " + winner + " wins!"
 }
 
-const App = () => {
+const range = n => Array.from(Array(n).keys())
+
+const players = (howManyPlayers, parent) => {
+  return range(howManyPlayers).map((i)=>
+    <Player key={i} id={i} parent={parent}/>)
+}
+
+const GenericApp = (howManyPlayers) => {
   const component = new React.Component()
   component.state = {
-    players: [0, 0],
-    winner: 0,
+    players: Array(howManyPlayers).fill(0),
+    winner: 0
   }
 
   component.render = () => {
     return (
       <div>
         <h1 className="Winner">{genScoreboardMessage(component.state.winner)}</h1>
-        <div className="Players">
-          <Player id={0} parent={component}/>
-          <Player id={1} parent={component}/>
-        </div>
+        <div className="Players">{players(howManyPlayers, component)}</div>
       </div>
     )
   }
   return component
 }
+
+const App = () => GenericApp(4)
 
 export default App;
