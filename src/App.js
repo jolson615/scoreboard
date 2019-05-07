23d7b2b6833a4643381e73bcf0dc4345dcee56cc
Julian <thebarbershaveshimself@172-29-247-217.dynapool.nyu.edu>
2019-05-07 13:55:41 -0400

    EASY

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ const genScoreboardMessage = winner => {
 const App = () => {
   const component = new React.Component()
   component.state = {
-    players: [0,0,0,0,0,0],
-    winner: 0
+    players: [0, 0],
+    winner: 0,
   }
 
   component.render = () => {
@@ -18,12 +18,8 @@ const App = () => {
       <div>
         <h1 className="Winner">{genScoreboardMessage(component.state.winner)}</h1>
         <div className="Players">
-          <Player key={0} id={0} parent={component}/>
-          <Player key={1} id={1} parent={component}/>
-          <Player key={2} id={2} parent={component}/>
-          <Player key={3} id={3} parent={component}/>
-          <Player key={4} id={4} parent={component}/>
-          <Player key={5} id={5} parent={component}/>
+          <Player id={0} parent={component}/>
+          <Player id={1} parent={component}/>
         </div>
       </div>
     )
