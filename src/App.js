6ef94a80bcdf083001af7f8b56d556c0222350f6
Julian <thebarbershaveshimself@172-29-247-217.dynapool.nyu.edu>
2019-05-08 13:52:39 -0400

    jolsen fixes :)

--- src/App.js
+++ src/App.js
@@ -3,10 +3,25 @@ import Player from './components/player'
 import './App.css';
 
 const genScoreboardMessage = winner => {
-  if (winner == -1) {
+  if (winner === -1) {
     return "It's a tie!"
   }
   return "Player " + winner + " wins!"
+}
+
+
+const getMax = arr => {
+  let maxVal = arr[0]
+  let maxIdx = 0
+  arr.slice(1).forEach((v, i) => {
+    if (v > maxVal) {
+      maxIdx = i
+      maxVal = v
+    } else if (v === maxVal) {
+      maxIdx = -1
+    }
+  })
+  return maxIdx
 }
 
 const App = () => {
@@ -16,13 +31,21 @@ const App = () => {
     winner: -1,
   }
 
+  const handleClick = (n, id) => {
+    component.setState(prevState => {
+      prevState.players[id] += n
+      prevState.winner = getMax(prevState.players)
+      return prevState
+    })
+  }
+
   component.render = () => {
     return (
       <div>
         <h1 className="Winner">{genScoreboardMessage(component.state.winner)}</h1>
         <div className="Players">
-          <Player id={0} parent={component}/>
-          <Player id={1} parent={component}/>
+        <Player id={0} scores={component.state.players} clickHandler={handleClick}/>
+        <Player id={1} scores={component.state.players} clickHandler={handleClick}/>
         </div>
       </div>
     )
--- src/components/player.js
+++ src/components/player.js
@@ -1,38 +1,22 @@
 import React from 'react';
 import './player.css';
 
-const getMax = arr => {
-  let maxVal = arr[0]
-  let maxIdx = 0
-  arr.slice(1).forEach((v, i) => {
-    if (v > maxVal) {
-      maxIdx = i
-      maxVal = v
-    } else if (v == maxVal) {
-      maxIdx = -1
-    }
-  })
-  return maxIdx
-}
-
 const Player = (props) => {
-  const handleClick = (n) => {
-    props.parent.setState(prevState => {
-      prevState.players[props.id] += n
-      prevState.winner = getMax(prevState.players)
-      return prevState
-    })
-  }
+  const component = new React.Component(props)
 
-  return (
-    <div className="Player">
-      <h1>{props.parent.state.players[props.id]}</h1>
-      <div>
-        <button onClick={()=>handleClick(-1)}>⬇️</button>
-        <button onClick={()=>handleClick(1)}>⬆️</button>
+  component.render = () => {
+    console.log(props)
+    return (
+      <div className="Player">
+        <h1>{props.scores[props.id]}</h1>
+        <div>
+          <button onClick={()=>props.clickHandler(-1, props.id)}>⬇️</button>
+          <button onClick={()=>props.clickHandler(1, props.id)}>⬆️</button>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
+  return component
 }
 
 export default Player;
