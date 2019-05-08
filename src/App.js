e688af5a16e3606c4af16d7d1478ec575f24e387
Julian <thebarbershaveshimself@172-29-247-217.dynapool.nyu.edu>
2019-05-08 12:47:01 -0400

    jolsen fixes

--- src/App.js
+++ src/App.js
@@ -13,13 +13,23 @@ const App = () => {
     winner: 0,
   }
 
+  const handleClick = (n, id) => {
+    component.setState(prevState => {
+      prevState.players[id] += n
+      if (prevState.players[id] > prevState.players[prevState.winner]) {
+        prevState.winner = id
+      }
+      return prevState
+    })
+  }
+
   component.render = () => {
     return (
       <div>
         <h1 className="Winner">{genScoreboardMessage(component.state.winner)}</h1>
         <div className="Players">
-          <Player id={0} parent={component}/>
-          <Player id={1} parent={component}/>
+          <Player id={0} score={component.state.players[0]} clickHandler={handleClick}/>
+          <Player id={1} score={component.state.players[1]} clickHandler={handleClick}/>
         </div>
       </div>
     )
--- src/components/player.js
+++ src/components/player.js
@@ -2,25 +2,19 @@ import React from 'react';
 import './player.css';
 
 const Player = (props) => {
-  const handleClick = (n) => {
-    props.parent.setState(prevState => {
-      prevState.players[props.id] += n
-      if (prevState.players[props.id] > prevState.players[prevState.winner]) {
-        prevState.winner = props.id
-      }
-      return prevState
-    })
-  }
+  const component = new React.Component()
 
-  return (
-    <div className="Player">
-      <h1>{props.parent.state.players[props.id]}</h1>
-      <div>
-        <button onClick={()=>handleClick(-1)}>⬇️</button>
-        <button onClick={()=>handleClick(1)}>⬆️</button>
+  component.render = () => {
+    return (
+      <div className="Player">
+        <h1>{props.score}</h1>
+        <div>
+          <button onClick={()=>props.clickHandler(-1, props.id)}>⬇️</button>
+          <button onClick={()=>props.clickHandler(1, props.id)}>⬆️</button>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default Player;
